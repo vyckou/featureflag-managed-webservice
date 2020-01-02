f7bd7fc545f5a29ea3235d8d8fb8a53ecd1cb1f7
Vytautas Maciulskis <vyma@danskebank.com>
2020-01-02 13:44:10 +0200

    a bit debug info in the stdout

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 
@@ -29,6 +30,7 @@ func list() (events.APIGatewayProxyResponse, error) {
 	isVersion2, _ := client.GetValue("version2", false).(bool)
 
 	if !isEnabled {
+		fmt.Println("get endpoint is not implemented")
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusNotImplemented,
 			Body:       "Not Implemented",
@@ -36,6 +38,7 @@ func list() (events.APIGatewayProxyResponse, error) {
 	}
 
 	if isVersion2 {
+		fmt.Println("get endpoint V2")
 		contact := contactv2{Name: "Jonas", PhoneNumber: "911", Birthday: "1990-11-11"}
 		js, _ := json.Marshal(contact)
 		return events.APIGatewayProxyResponse{
@@ -46,6 +49,7 @@ func list() (events.APIGatewayProxyResponse, error) {
 
 	contact := contactv1{Name: "Jonas", PhoneNumber: "911"}
 	js, _ := json.Marshal(contact)
+	fmt.Println("get endpoint V1")
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
 		Body:       string(js),
